924d33229c684641868d0e699a2cc8a8051ba6d9
skyasu <skyasu@naver.com>
2025-05-27 00:06:11 +0900

    AdminDashboardCharts 컴포넌트 완성 - Recharts 기반 실시간 시각화 대시보드

---
+++ src/app/admin/charts/page.tsx
@@ -0,0 +1,9 @@
+import AdminDashboardCharts from '@/components/AdminDashboardCharts';
+
+export default function AdminChartsPage() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <AdminDashboardCharts />
+    </div>
+  );
+} 
